3e88812fb72f7fa7d5454e64072dd1ea1751e0ef
Fadhlaoui <fadhlaouiraied@hotmail.com>
2020-04-10 21:23:43 +0100

    fixes in switchMap

--- src/app/admin-auth-guard.service.ts
+++ src/app/admin-auth-guard.service.ts
@@ -3,15 +3,13 @@ import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/switchMap';
+
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AdminAuthGuardService implements CanActivate {
   constructor(private auth: AuthService, private userService: UserService) {}
   canActivate(): Observable<boolean> {
-    return this.auth.user$
-      .switchMap((user) => this.userService.get(user.uid))
-      .map((appUser: any) => appUser.isAdmin);
+    return this.auth.appUser$.map((appUser: any) => appUser.isAdmin);
   }
 }
