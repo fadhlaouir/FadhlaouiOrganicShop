128cd15f7ee4274ffaab9f4109e36366b1a526a4
Fadhlaoui <fadhlaouiraied@hotmail.com>
2020-04-11 14:38:41 +0100

    Displaying the list of products from firebase

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { ProductService } from './../../product.service';
 import { CategoryService } from './../../category.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -10,13 +11,15 @@ import { Component, OnInit } from '@angular/core';
 export class ProductFormComponent implements OnInit {
   categories$;
   constructor(
-    categoryService: CategoryService,
+    private router: Router,
+    private categoryService: CategoryService,
     private productService: ProductService
   ) {
     this.categories$ = categoryService.getCategories();
   }
   save(product) {
     this.productService.create(product);
+    this.router.navigate(['/admin/products']);
   }
   ngOnInit() {}
 }
